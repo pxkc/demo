25d46a7115453479eacd718073c75ddb8c2af751
pxk <pxk4464@qq.com>
2019-10-19 01:29:54 +0800

    使用git中的commit

--- src/main/java/com/nowcoder/demo/dao/NewsDao.java
+++ src/main/java/com/nowcoder/demo/dao/NewsDao.java
@@ -17,7 +17,7 @@ public interface NewsDao {
     String TABLE_NAME = "news";
     String INSERT_FIELDS = " title, link, image, like_count, comment_count, create_date, user_id ";
     String SELECT_FIELDS = " id, " + INSERT_FIELDS;
-
+//
     @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
             ") values (#{title},#{link},#{image},#{likeCount},#{commentCount},#{createDate},#{userId})"})
     int addNews(News news);
